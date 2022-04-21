5f0e4ec12e0ec15ebc221d7b83db572f8ef6b99e
ikovalev <ilyaKovalev@x5.ru>
2022-04-21 18:24:10 +0300

    add mock

--- src/pages/DepartmentsPage.js
+++ src/pages/DepartmentsPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 import {Breadcrumb} from '../components/Breadcrumb';
+import {departments} from '../utils/mock';
 
 const DepartmentsPage = () => {
   return (
     <div className="departments-page">
       <Breadcrumb />
-      departments-page
+
+      <ListGroup as="ol" numbered>
+        {departments.map((department) => (
+          <ListGroup.Item key={department} action as="li" className="d-flex justify-content-between align-items-start">
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">{department}</div>
+            </div>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   );
 };
