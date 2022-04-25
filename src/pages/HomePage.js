f5e2c301c5f437153c7ba01a8596547046aff6e0
ikovalev <ilyaKovalev@x5.ru>
2022-04-25 22:30:02 +0300

    🔒️ home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,19 +4,27 @@ import Carousel from 'react-bootstrap/Carousel';
 const HomePage = () => {
   return (
     <div className="home-page">
-      <Carousel className="home-page__carousel">
-        <Carousel.Item className="home-page__carousel-item a1">
+      <Carousel className="home-page__carousel" variant="dark">
+        <Carousel.Item className="home-page__carousel-item">
           <Carousel.Caption>
-            <h3>Подсистема аналитики для автоматизированной системы управления образовательным процессом</h3>
-            <p>"Открытый цифровой университет"</p>
+            <div>
+              <h3 className="text-color fw-normal">
+                Подсистема аналитики для автоматизированной системы управления образовательным процессом
+              </h3>
+              <h3 className="text-color__active fw-bold">"Открытый цифровой университет"</h3>
+            </div>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item className="home-page__carousel-item a2">
+        <Carousel.Item className="home-page__carousel-item">
           <Carousel.Caption>
-            <h3>Данный функционал предназначен для отображения статистики:</h3>
-            <p>аттестаций, стипендий, сессий</p>
-            <h3>Уровни отображения:</h3>
-            <p>университет, институт, кафедра, группа.</p>
+            <h3 className="text-color fw-normal">
+              Данный функционал предназначен для отображения статистики:
+              <span className="text-color__active fw-bold"> аттестаций, стипендий, сессий</span>.
+            </h3>
+            <h3 className="text-color fw-normal">
+              Уровни отображения:
+              <span className="text-color__active fw-bold"> университет, институт, кафедра, группа</span>.
+            </h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
