56827c5bf0497a715d7296559e9c55fc13847ac5
ikovalev <ilyaKovalev@x5.ru>
2022-04-25 23:12:52 +0300

    🚑 change readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+### `npm i`
+
 ### `npm start`
+
+<p><a href="https://ilya1995.github.io/university/">Тут</a> можно посмотреть</p>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
-const HomePage = () => {
-  return (
-    <div className="home-page">
-      <Carousel className="home-page__carousel" variant="dark">
-        <Carousel.Item className="home-page__carousel-item">
-          <Carousel.Caption>
-            <div>
-              <h3 className="text-color fw-normal">
-                Подсистема аналитики для автоматизированной системы управления образовательным процессом
-              </h3>
-              <h3 className="text-color__active fw-bold">"Открытый цифровой университет"</h3>
-            </div>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item className="home-page__carousel-item">
-          <Carousel.Caption>
+const HomePage = () => (
+  <div className="home-page">
+    <Carousel className="home-page__carousel" variant="dark">
+      <Carousel.Item className="home-page__carousel-item">
+        <Carousel.Caption>
+          <div>
             <h3 className="text-color fw-normal">
-              Данный функционал предназначен для отображения статистики:
-              <span className="text-color__active fw-bold"> аттестаций, стипендий, сессий</span>.
+              Подсистема аналитики для автоматизированной системы управления образовательным процессом
             </h3>
-            <h3 className="text-color fw-normal">
-              Уровни отображения:
-              <span className="text-color__active fw-bold"> университет, институт, кафедра, группа</span>.
-            </h3>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
-  );
-};
+            <h3 className="text-color__active fw-bold">"Открытый цифровой университет"</h3>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item className="home-page__carousel-item">
+        <Carousel.Caption>
+          <h3 className="text-color fw-normal">
+            Данный функционал предназначен для отображения статистики:
+            <span className="text-color__active fw-bold"> аттестаций, стипендий, сессий</span>.
+          </h3>
+          <h3 className="text-color fw-normal">
+            Уровни отображения:
+            <span className="text-color__active fw-bold"> университет, институт, кафедра, группа</span>.
+          </h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  </div>
+);
 
 export {HomePage};
